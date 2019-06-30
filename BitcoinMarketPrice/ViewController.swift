7ee1362fa87655e48c3a4c8459986a334ad17c44
Glauco Dantas Beserra <dantas.glauco@gmail.com>
2019-06-30 20:39:17 -0300

    Pod init
    Podfile added to the project and charts library installed

--- BitcoinMarketPrice/ViewController.swift
+++ BitcoinMarketPrice/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let blockchainClient = BlockchainClient()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    private func getMarketPrice(at timespan: Timespan) {
+        blockchainClient.getMarketPrice(at: timespan) { result in
+            switch result {
+            case .success(let marketPrice):
+                guard let history = marketPrice?.values else { return }
+                print(history)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
 
