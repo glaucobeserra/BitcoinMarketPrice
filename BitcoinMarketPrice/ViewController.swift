fd13b85821fb1e46fb49551c9034f2cbf6b03dfa
Glauco Dantas Beserra <dantas.glauco@gmail.com>
2019-07-01 07:22:25 -0300

    Configuring chart

--- BitcoinMarketPrice/ViewController.swift
+++ BitcoinMarketPrice/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var marketPriceDayLabel: UILabel!
     @IBOutlet weak var marketPriceDateLabel: UILabel!
     
+    @IBOutlet weak var lineChartView: LineChartView!
+    var chartDescription = ""
+    var values: [Value] = []
     private let blockchainClient = BlockchainClient()
+    
+    lazy var formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 1
+        formatter.negativeSuffix = " $"
+        formatter.positiveSuffix = " $"
+        
+        return formatter
+    }()
+    
+    lazy var newFormatter: NumberFormatter = {
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale.current
+        
+        return currencyFormatter
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +46,7 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        getMarketPrice(at: .lastDay)
+        getMarketPrice(at: .lastMonth)
     }
 
     
@@ -32,9 +54,16 @@ class ViewController: UIViewController {
         blockchainClient.getMarketPrice(at: timespan) { result in
             switch result {
             case .success(let marketPrice):
-                guard let history = marketPrice?.values else { return }
-                guard let value = history.first else {return}
-                self.refreshValues(with: value)
+                
+                guard let marketPrice = marketPrice else { return }
+                let values = marketPrice.values
+                
+                self.values = values
+                self.chartDescription = marketPrice.description
+                
+                guard let lastValue = values.last else {return}
+                self.refreshValues(with: lastValue)
+                self.updateGraph()
                 
             case .failure(let error):
                 print(error)
@@ -55,6 +84,38 @@ class ViewController: UIViewController {
             self.marketPriceDateLabel.text = completeDate
             print(completeDate)
         }
+    }
+    
+    func updateGraph(){
+        var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
+        
+        let leftAxis = lineChartView.leftAxis
+        leftAxis.valueFormatter = DefaultAxisValueFormatter(formatter: formatter)
+//        leftAxis.axisMinimum = 0
+        lineChartView.backgroundColor = .white
+        
+        
+        lineChartView.rightAxis.enabled = false
+        
+        //here is the for loop
+        for (i, value) in values.enumerated() {
+            let value = ChartDataEntry(x: Double(i), y: value.usd) // here we set the X and Y status in a data chart entry
+            lineChartEntry.append(value) // here we add it to the data set
+        }
+        
+        let line1 = LineChartDataSet(entries: lineChartEntry, label: "Bitcoin Market Price (USD)")
+        
+//        let line1 = LineChartDataSet(values: lineChartEntry, label: "Number") //Here we convert lineChartEntry to a LineChartDataSet
+        line1.colors = [.blue] //Sets the colour to blue
+        line1.circleRadius = 0.5
+        line1.mode = .cubicBezier
+        
+        let data = LineChartData() //This is the object that will be added to the chart
+        data.addDataSet(line1) //Adds the line to the dataSet
+        data.setValueFormatter(DefaultValueFormatter(formatter: newFormatter))
+        
+        lineChartView.data = data //finally - it adds the chart data to the chart and causes an update
+        lineChartView.chartDescription?.text = chartDescription // Here we set the description for the graph
     }
 
 }
